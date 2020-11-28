1e0c1d459df4a0f1880de71fc7baeb4308d2be3e
Aga <agnesseweryn@gmail.com>
2020-11-28 18:38:48 +0100

    Shift Arrays

---
+++ shiftArray.go
@@ -0,0 +1,21 @@
+package shiftArray
+
+func ShiftKTimes(A []int, K int) []int {
+    if len(A) < 2 {
+        return A
+    }
+
+    for i := 0; i < K; i++ {
+        ShiftOnce(A)
+    }
+
+    return A
+}
+
+func ShiftOnce(A []int){
+    prev := A[0]
+    for i := 1; i < len(A); i++ {       
+        A[i], prev = prev, A[i]
+    }
+    A[0] = prev
+}
